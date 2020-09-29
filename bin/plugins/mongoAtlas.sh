d8cf64a4682b06a85b410b52b516afff5ed27e6a
GuillaumeWaignier <Guillaume.Waignier@gmail.com>
2020-09-29 11:30:11 +0200

    feat(mongoAtlas): handle right 'readAnyDatabase' and enrich node with collectionCount, indexSize, ...

--- bin/plugins/mongoAtlas.sh
+++ bin/plugins/mongoAtlas.sh
@@ -13,6 +13,16 @@ function Mongodb_atlas_list_user {
   MONGO_USERS=`curl -s -u "${MONGODB_ATLAS_USER}:${MONGODB_ATLAS_PASS}" --digest -XGET ${MONGODB_ATLAS_URL}/api/atlas/v1.0/groups/${MONGODB_ATLAS_PROJECT_ID}/databaseUsers?itemsPerPage=499`
 }
 
+function Mongodb_atlas_create_read_link_any {
+  k=0
+  while [ "$k" -lt "${MONGO_DATABASES_COUNT}" ]
+  do
+    NAME=`echo "${MONGO_DATABASES}" | jq -r ".results[${k}].databaseName"`
+    echo "{\"sourceType\":\"mongo\",\"sourceName\":\"${NAME}\",\"targetType\":\"app\",\"targetName\":\"${1}\",\"linkName\":\"read\",\"linkProperties\":\"\"}" >> ${OUTPUT_FILE}
+    k=$(( k+1 ))
+  done
+}
+
 
 function collect_mongo_user {
 
@@ -33,6 +43,9 @@ function collect_mongo_user {
     then
       echo "{\"sourceType\":\"mongo\",\"sourceName\":\"${MONGO_USER_DB}\",\"targetType\":\"app\",\"targetName\":\"${MONGO_USER_NAME}\",\"linkName\":\"read\",\"linkProperties\":\"\"}" >> ${OUTPUT_FILE}
       echo "{\"sourceType\":\"app\",\"sourceName\":\"${MONGO_USER_NAME}\",\"targetType\":\"mongo\",\"targetName\":\"${MONGO_USER_DB}\",\"linkName\":\"write\",\"linkProperties\":\"\"}" >> ${OUTPUT_FILE}
+    elif [ "${MONGO_USER_RIGHT=}" == "readAnyDatabase" ]
+    then
+      Mongodb_atlas_create_read_link_any "${MONGO_USER_NAME}"
     else
       echo "[MongoDBAtlas] Role ${MONGO_USER_RIGHT} ignored for user ${MONGO_USER_NAME}"
     fi
@@ -61,7 +74,44 @@ function Mongodb_atlas_create_link {
   done
 }
 
+function Mongodb_atlas_list_database {
+  echo "[MongoDBAtlas] Get all Databases"
+  MONGO_ALL_PROCESS=`curl -s -u "${MONGODB_ATLAS_USER}:${MONGODB_ATLAS_PASS}" --digest -XGET ${MONGODB_ATLAS_URL}/api/atlas/v1.0/groups/${MONGODB_ATLAS_PROJECT_ID}/processes`
+  MONGO_PROCESS=`echo "${MONGO_ALL_PROCESS}" | jq -r ".results[0].id"`
 
+  MONGO_DATABASES=`curl -s -u "${MONGODB_ATLAS_USER}:${MONGODB_ATLAS_PASS}" --digest -XGET ${MONGODB_ATLAS_URL}/api/atlas/v1.0/groups/${MONGODB_ATLAS_PROJECT_ID}/processes/${MONGO_PROCESS}/databases`
+  MONGO_DATABASES_COUNT=`echo "${MONGO_DATABASES}" | jq ".totalCount"`
+}
+
+
+function Mongodb_atlas_enrich_node {
+
+  echo "[MongoDBAtlas] There is ${MONGO_DATABASES_COUNT} databases (enriche)"
+
+  k=0
+  while [ "$k" -lt "${MONGO_DATABASES_COUNT}" ]
+  do
+
+    echo "[MongoDBAtlas] Collect databases ${k}/${MONGO_DATABASES_COUNT}"
+
+    NAME=`echo "${MONGO_DATABASES}" | jq -r ".results[${k}].databaseName"`
+    MONGO_DATABASES_INFO=`curl -s -u "${MONGODB_ATLAS_USER}:${MONGODB_ATLAS_PASS}" --digest -XGET ${MONGODB_ATLAS_URL}/api/atlas/v1.0/groups/${MONGODB_ATLAS_PROJECT_ID}/processes/${MONGO_PROCESS}/databases/${NAME}/measurements?granularity=PT5M\&period=PT10M`
+
+    MONGO_COLLECTION_COUNT=`echo "${MONGO_DATABASES_INFO}" | jq ".measurements | map(select(.name == \"DATABASE_COLLECTION_COUNT\" ))[].dataPoints[0].value"`
+    MONGO_INDEX_COUNT=`echo "${MONGO_DATABASES_INFO}" | jq ".measurements | map(select(.name == \"DATABASE_INDEX_COUNT\" ))[].dataPoints[0].value"`
+    MONGO_OBJECT_COUNT=`echo "${MONGO_DATABASES_INFO}" | jq ".measurements | map(select(.name == \"DATABASE_OBJECT_COUNT\" ))[].dataPoints[0].value"`
+    MONGO_INDEX_SIZE=`echo "${MONGO_DATABASES_INFO}" | jq ".measurements | map(select(.name == \"DATABASE_INDEX_SIZE\" ))[].dataPoints[0].value"`
+    MONGO_DATA_SIZE=`echo "${MONGO_DATABASES_INFO}" | jq ".measurements | map(select(.name == \"DATABASE_DATA_SIZE\" ))[].dataPoints[0].value"`
+
+    echo "mongo;${NAME};{indexCount:${MONGO_INDEX_COUNT},indexSize:${MONGO_INDEX_SIZE},docCount:${MONGO_OBJECT_COUNT},docSize:${MONGO_DATA_SIZE},collectionCount:${MONGO_COLLECTION_COUNT}}" >> ${ENRICHED_NODES_FILE}
+    k=$(( k+1 ))
+  done
+}
+
+
+
+Mongodb_atlas_list_database
 Mongodb_atlas_list_user
 Mongodb_atlas_create_link
+Mongodb_atlas_enrich_node
 
